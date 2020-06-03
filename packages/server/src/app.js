0ee894b2cd74fa179b2ff82db21a2f5f8be5252e
igorspawnbuk <igorspawnbuk@mail.ru>
2020-06-03 21:20:57 +0300

    fix dependencies

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,6 +1,5 @@
 const calculateCartPrice = require('./calculateCartPrice')
 const express = require('express')
-require('dotenv').config()
 const cors = require('cors')
 const axios = require('axios')
 const app = express()
